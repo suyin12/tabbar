a991bed8ac239ad631132572fec87f333e6d8394
sujianhui <sujianhui@beyondsoft.com>
2021-04-02 17:37:18 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <tab-bar>
+      <tab-bar-item>
+        <div slot="item-icon" v-if="!isActive"><img src="./assets/img/tabbar/home.svg" /></div>
+        <div slot="item-icon" v-else><img src="./assets/img/tabbar/home_active.svg" /></div>
+        <div slot="item-icon">首页</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <div slot="item-icon" v-if="!isActive"><img src="./assets/img/tabbar/category.svg" /></div>
+        <div slot="item-icon" v-else><img src="./assets/img/tabbar/category_active.svg" /></div>
+        <div slot="item-icon">分类</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <div slot="item-icon" v-if="!isActive"><img src="./assets/img/tabbar/shopcart.svg" /></div>
+        <div slot="item-icon" v-else><img src="./assets/img/tabbar/shopcart_active.svg" /></div>
+        <div slot="item-icon">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <div slot="item-icon" v-if="!isActive"><img src="./assets/img/tabbar/profile.svg" /></div>
+        <div slot="item-icon" v-else><img src="./assets/img/tabbar/profile_active.svg" /></div>
+        <div slot="item-icon">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from './components/tabbar/TabBar'
+import TabBarItem from "./components/tabbar/TabBarItem";
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      isActive: true
+    }
+  },
+  components: {
+    TabBarItem,
+    TabBar
+  }
+}
+</script>
+<!--vue 文件中可以用 @import 引入css文件-->
+<style>
+  @import "./assets/css/base.css";
+</style>
---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="tab-bar-item">
+        <slot name="item-icon"></slot>
+        <slot name="item-text"></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TabBarItem"
+    }
+</script>
+
+<style scoped>
+    .tab-bar-item {
+        flex: 1;
+        text-align: center;
+        height: 49px;
+        margin-top: 3px;
+        font-size: 14px;
+        margin-bottom: 1px;
+    }
+
+    .tab-bar-item img {
+        width: 29px;
+        height: 29px;
+        vertical-align: middle;
+    }
+</style>
